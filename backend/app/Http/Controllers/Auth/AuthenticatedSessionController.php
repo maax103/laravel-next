05787c0949630571510d13d22f4087f20c0d5426
Maximiliano Furtado <maximilianomfurtado@gmail.com>
2025-02-01 00:47:30 -0300

    Changes auth and removes web routes

--- backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,35 +4,20 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Handle an incoming authentication request.
-     */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): JsonResponse
     {
         $request->authenticate();
 
-        $request->session()->regenerate();
+        $user = $request->user();
+        $token = $user->createToken('api-token')->plainTextToken;
 
-        return response()->noContent();
-    }
-
-    /**
-     * Destroy an authenticated session.
-     */
-    public function destroy(Request $request): Response
-    {
-        Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        return response()->noContent();
+        return response()->json([
+            'token' => $token,
+            'user' => $user,
+        ]);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\MusicController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+Route::post('/music-import', [MusicController::class, 'importMusic'])
+    ->middleware(['auth:sanctum']);
+
+Route::post('/music/{musicId}/user/{userId}', [MusicController::class, 'associateUserMusic'])
+    ->middleware(['auth:sanctum']);
+
+Route::get('/music-users', [MusicController::class, 'getUserMusic'])
+    ->middleware(['auth:sanctum']);
+
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})
+    ->middleware(['auth:sanctum']);
+
+require __DIR__.'/auth.php';
